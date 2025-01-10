f1729e9f1cb640274c423f54a90fb7926aed8abb
Jiachi Liu <inbox@huozhi.im>
2025-01-10 01:40:59 +0100

    feat: highlight line number

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
-import { 
-  tokenize, 
-  generate,
-  type HighlightOptions,
-} from 'sugar-high'
+import { tokenize, generate, type HighlightOptions } from 'sugar-high'
 import { map as unistMap } from 'unist-util-map'
 import rangeParser from 'parse-numeric-range'
 import * as languagesOptions from './lang'
+
+function cx(...args: any[]): string {
+  return args.filter(Boolean).join(' ')
+}
+
+type HighlightRange = number | [number, number]
+
+/**
+ * Parses highlight information from the `meta` field of a Remark code block node.
+ * @param meta The `meta` string containing highlight information, e.g., "{1,3-5}".
+ * @returns An array of highlight ranges. Single numbers represent single lines, and tuples represent ranges.
+ */
+function parseHighlightMeta(meta?: string): HighlightRange[] {
+  if (!meta) return []
+
+  const highlightRegex = /{([\d,-]+)}/ // Matches {1,3-5}
+  const match = meta.match(highlightRegex)
+
+  if (!match) return []
+
+  const ranges = match[1].split(',').map((range) => {
+    if (range.includes('-')) {
+      const [start, end] = range.split('-').map(Number)
+      return [start, end] as [number, number]
+    }
+    return Number(range)
+  })
+
+  return ranges
+}
 
 const parseLang = (str) => {
   const match = (regexp) => {
@@ -49,7 +75,9 @@ const highlight = () => (tree) => {
 
     const { lang } = parseLang(node.lang)
 
-    let options: HighlightOptions | undefined  = undefined
+    const highlightRanges = parseHighlightMeta(node.meta)
+
+    let options: HighlightOptions | undefined = undefined
 
     if (lang in languagesOptions) {
       options = languagesOptions[lang]
@@ -63,9 +91,36 @@ const highlight = () => (tree) => {
         .pop()
 
     const childrenLines = generate(tokenize(codeText, options))
-    
+
+    let lineIndex = 1
     for (const line of childrenLines) {
-      line.children.push(h('span', { className: 'sh__token--line' }, [{ type: 'text', value: '\n' }]))
+      // if it's highlighted lines, add a classname `sh__line--highlighted`
+      let isCurrentLineHighlighted = highlightRanges.includes(lineIndex)
+      
+
+      let highLightClassName = ''
+      if (isCurrentLineHighlighted) {
+        highLightClassName = 'sh__line--highlighted'
+      }
+
+      // add line break
+      line.children.push(
+        h(
+          'span',
+          {
+            className: `sh__token--line`,
+          },
+          [{ type: 'text', value: '\n' }]
+        )
+      )
+
+      // add class to line
+      line.properties.className = cx(
+        line.properties.className,
+        highLightClassName
+      )
+
+      lineIndex++
     }
 
     const code = h(
